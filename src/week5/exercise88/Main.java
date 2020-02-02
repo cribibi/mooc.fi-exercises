706bc88d586f01ce556282c60de3461f443cd0fa
cribibi <cricler_bianca_melania@yahoo.com>
2020-02-02 22:18:36 +0100

    first commit

---
+++ src/week5/exercise88/Main.java
@@ -0,0 +1,11 @@
+package week5.exercise88;
+
+public class Main {
+    public static void main(String[] args) {
+        Student pekka = new Student("Pekka Mikkola", "013141590");
+        System.out.println("name: " + pekka.getName());
+        System.out.println("studentnumber: " + pekka.getStudentNumber());
+        System.out.println(pekka);
+    }
+
+}
---
+++ src/week5/exercise88/Student.java
@@ -0,0 +1,26 @@
+package week5.exercise88;
+
+public class Student {
+    String name;
+    String studentNumber;
+
+    public Student(String name, String studentNumber) {
+        this.name = name;
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("");
+        sb.append(name).append(" (").append(studentNumber).append(")");
+        return sb.toString();
+    }
+}
