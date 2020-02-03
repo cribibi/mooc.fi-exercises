0e5b20648698aea96cc33b956be502c79b0ca260
cribibi <cricler_bianca_melania@yahoo.com>
2020-02-03 07:34:42 +0100

    3rd commit

--- src/week4/Exercise73.java
+++
@@ -1,8 +0,0 @@
-package week4;
-
-public class Exercise73 {
-    public static void main(String[] args) {
-        Exercise73Product produs=new Exercise73Product("Banana", 1.2, 13);
-        produs.printProduct();
-    }
-}
--- src/week5/Exercise89.java
+++
@@ -1,7 +0,0 @@
-package week5;
-
-public class Exercise89 {
-    public static void main(String[] args) {
-
-    }
-}
