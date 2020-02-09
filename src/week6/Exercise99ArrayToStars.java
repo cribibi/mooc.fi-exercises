f0ae3a8a13decf820417005327573e9772392eed
cribibi <cricler_bianca_melania@yahoo.com>
2020-02-09 13:37:41 +0200

    commit ex 99

--- src/week6/Exercise99ArrayToStars.java
+++ src/week6/Exercise99ArrayToStars.java
@@ -1,19 +1,23 @@
 package week6;
 
 public class Exercise99ArrayToStars {
-        public static void main(String[] args) {
-            int[] array = {5, 1, 3, 4, 2};
-            printArrayAsStars(array);
-        }
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayAsStars(array);
+    }
 
-        public static void printArrayAsStars(int[] array) {
-            for (int i = 0; i <5 ; i++) {
-                for (int j = 0; j <array[j] ; j++) {
-                    System.out.print("*");
-                }
-
+    public static void printArrayAsStars(int[] array) {
+        for (int i = 0; i < 5; i++) {
+            int j = 0;
+            while (j < array[i]) {
+                System.out.print("*");
+                j++;
             }
+            System.out.println();
+
         }
     }
+}
+
 
 
